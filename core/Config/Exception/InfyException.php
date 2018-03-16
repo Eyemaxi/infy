97daa07a66fb11b236a480aebd23a7604e4e3398
Max Glazkov <maksimglaz@gmail.com>
2018-03-16 20:17:25 +0200

    update

---
+++ core/Config/Exception/InfyException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev01
+ * Date: 16.03.18
+ * Time: 19:53
+ */
+
+namespace Infy\Core\Config\Exception;
+
+
+class InfyException extends \Exception
+{
+
+}
