5a9f81f00b5eff3f880769ff59cd2db33345b871
Max Glazkov <maksimglaz@gmail.com>
2018-05-24 19:58:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
  * @copyright Copyright (c) 2018 Infy
  * @license   https://www.infy-team.com/license.txt
  */
-
 // 1. General settings
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -19,4 +18,7 @@ require ROOT . '/vendor/autoload.php';
 // 3. Install connecting to the DB
 
 // 4. Start
-Infy\Core\Bootstrap::start();
+
+$bootstrap = \Infy\Core\Bootstrap::create();
+$bootstrap->install();
+$bootstrap->start();
